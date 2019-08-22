00d21ff54d3026332b398b4127872721634f7adc
Ted Cook <teodoro.cook@gmail.com>
2019-08-22 13:58:36 -0600

    Remove stale completion file references

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks"
 DEPS="gitlab.com/shellm/core"
-BASH_COMPLETIONS="cmp/awx-update-projects.completion.bash:cmp/awx-run-templates.completion.bash"
+BASH_COMPLETIONS=""
