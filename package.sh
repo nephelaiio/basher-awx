25238e283836a40f4dfa7cd6c7dd992bd0c0e8b5
Ted Cook <teodoro.cook@gmail.com>
2019-08-26 18:53:50 -0600

    Add inventory update script to basher bin list

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
-BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks"
+BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks:bin/awx-update-inventories"
 DEPS="gitlab.com/shellm/core"
 BASH_COMPLETIONS=""
