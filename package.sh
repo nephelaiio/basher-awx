7a701a73c9a97f2eb921700dff8ed31cc74260a2
Ted Cook <teodoro.cook@gmail.com>
2020-03-30 11:45:10 -0600

    Move shellm dependency reference to new github url

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks:bin/awx-update-inventories"
-DEPS="gitlab.com/shellm/core"
+DEPS="github.com/shellm-org/core"
 BASH_COMPLETIONS=""
