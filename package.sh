bfbd3f48a066756af3611a1f39e6cd20e0f202b0
Ted Cook <teodoro.cook@gmail.com>
2021-09-30 14:48:32 -0600

    Update basher bin list

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
-BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks:bin/awx-update-inventories"
+BINS="bin/awx-update-projects:bin/awx-run-templates:bin/awx-run-playbooks:bin/awx-update-inventories:bin/awx-update-inventory"
 DEPS="github.com/shellm-org/core"
 BASH_COMPLETIONS=""
